687ff36bb4e51b091a0212d946a77a1ae542f062
judith <judith.bello.torralbo@gmail.com>
2024-10-01 18:08:19 +0200

    Realizada la función findByName()

--- ejemplo_proyecto/src/Controller/NurseController.php
+++ ejemplo_proyecto/src/Controller/NurseController.php
@@ -25,4 +25,24 @@ class NurseController extends AbstractController
     }
 
 
+    
+    #[Route('/nurse/name/{name}', name: 'findByName', methods: ['GET'])]
+    public function findByName(string $name): Response {
+        $nurses = [
+            ["id" => 1, "nombre" => "Juan", "correo" => "juan@gmail.com", "password" => "1234"],
+            ["id" => 2, "nombre" => "Maria", "correo" => "Maria@gmail.com", "password" => "1234"],
+            ["id" => 3, "nombre" => "Pepa", "correo" => "Pepa@gmail.com", "password" => "1234"],
+            ["id" => 4, "nombre" => "Marc", "correo" => "Marc@gmail.com", "password" => "1234"]
+        ];
+
+        for($i=0; $i < count($nurses); $i++) {
+            if(in_array($name, $nurses[$i])) {
+                return new Response(json_encode($nurses[$i]), Response::HTTP_OK, ['Content-Type'=> 'application/json']);
+            } 
+        }
+
+        //si no lo ha encontrado en el bucle for
+        return new Response('No se ha encontrado el nombre', Response::HTTP_OK);
+    }
+
 }
